91f85ada5d600f8e19ced3a7e080cf1fcb24ba29
Abhishek Verma <av4programming@gmail.com>
2020-04-21 13:45:12 +0530

    final commit

--- app/views.py
+++ app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 
 from .servers import get_world_data
 
-
 def home(request):
     data = get_world_data()
     return render(request, "home.html", {"data": data})
--- static_proj/js/main.js
+++ static_proj/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(f => {
+
   const sort_by = (field, reverse, primer) => {
     const key = primer ?
     function(x) {
@@ -29,7 +30,7 @@ $(document).ready(f => {
   function search(nameKey, myArray){
     obj = [];
     for (var i=0; i < myArray.length; i++) {
-        if(myArray[i].Country.toLocaleLowerCase().indexOf(nameKey) != -1){
+        if(myArray[i].Country.toLowerCase().indexOf(nameKey) != -1){
           obj.push(myArray[i])
         }
     }
@@ -37,23 +38,27 @@ $(document).ready(f => {
   }
 
     var typingTimer;
+    var sel = $("#corona-data");
+    var s = $("div.world-data");
+    var countries = 'Loading...';
     var doneInterval = 2000;
     var searchInput = $("#table-search-form input[type=text]");
     var searchQuery;
     searchInput.keyup(function(e) {
-      searchQuery = $(this).val();
+      searchQuery = $(this).val().toLowerCase();
       clearTimeout(typingTimer)
-      typingTimer = setTimeout(fetchData(searchQuery), doneInterval)
+      typingTimer = setTimeout(showData(search(searchQuery, countries)), doneInterval)
     });
 
     searchInput.keydown(function(e) {
       clearTimeout(typingTimer);
     });
 
+    sel.append(countries)
+    s.append('Loading...<i class="fa fa-spinner fa-spin"></i>')
 
   const showData = (arr) => {
-    var sel = $("#corona-data")
-    sel.empty()
+    sel.empty();
     $.each(arr, f = (key, value) => {
       if(value.TotalConfirmed > 0) {
         var country = value.Country
@@ -83,26 +88,47 @@ $(document).ready(f => {
         )
 
       }
+    });
+  }
+
+  function getWorldData() {
+    $.ajax({
+      url: "https://api.covid19api.com/world/total",
+      method: "GET",
+      success: f = (data) => {
+        s.empty();
+        s.append(
+          '<ul class="list-group list-group-flush my-4">'+
+          "<li class='list-group-item center list-group-item-info lead'>TOTAL CONFIRMED CASES</li>"+
+          "<li class='list-group-item center list-group-item-info lead'>"+ numberWithCommas(data.TotalConfirmed) +"</li>"+
+          "<li class='list-group-item center list-group-item-danger lead'>TOTAL DEATHS</li>"+
+          "<li class='list-group-item center list-group-item-danger lead'>"+ numberWithCommas(data.TotalDeaths) +"</li>"+
+          "<li class='list-group-item center list-group-item-success lead'>TOTAL RECOVERED CASES</li>"+
+          "<li class='list-group-item center list-group-item-success lead'>"+ numberWithCommas(data.TotalRecovered) +"</li></ul>"
+        );
+      },
+      error: f = (error) => {
+        console.log(error)
+      }
     })
   }
 
-  const fetchData = (q=null) => {
+  getWorldData()
+
+  function fetchData () {
     $.ajax({
     url: "https://api.covid19api.com/summary",
     method: "GET",
     success: f = (data) => {
       countries = data.Countries;
       countries.sort(sort_by("TotalConfirmed", true, parseInt));
-      if (q) {
-        countries = search(q, countries);
-      }
       showData(countries);
     },
     error: f = (error) => {
-      console.log(error)
+      console.log(error);
     }
-  })
+  });
   }
 
-    fetchData()
+  fetchData();
 })
