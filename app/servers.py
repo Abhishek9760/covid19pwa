d15f9f60aa528670a38c0a372d7b4f2381c49a69
Abhishek Verma <av4programming@gmail.com>
2020-04-22 11:01:21 +0530

    Speed

--- app/servers.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-def get_world_data():
-    data = requests.get("https://api.covid19api.com/world/total").json()
-    return data
--- app/views.py
+++ app/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-from .servers import get_world_data
 
 def home(request):
-    data = get_world_data()
-    return render(request, "home.html", {"data": data})
+    return render(request, "home.html")
